ae319ea33827b8b70192f60d202e241af7190003
Abir Chodha <abirchodha1@gmail.com>
2025-07-09 03:05:36 +0530

    Add controller function to get all jobs in jobs route

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,18 @@
+import Job from "../models/Job.js";
+
 // Get All Jobs
-export const getJobs = async (req, res) => {};
+export const getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({ visible: true }).populate({
+      path: "companyId",
+      select: "-password",
+    });
+
+    res.json({ success: true, jobs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Get Single Job Using JobID
 export const getJobById = async (req, res) => {};
