e0d38c7ec2dc360032119158adcca2127802c548
Abir Chodha <abirchodha1@gmail.com>
2025-07-10 03:30:39 +0530

    Add user controller func to update resume

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import JobApplication from "../models/JobApplication.js";
 import Job from "../models/Job.js";
+import { v2 as cloudinary } from "cloudinary";
 
 // Get user data
 export const getUserData = async (req, res) => {
@@ -75,4 +76,20 @@ export const getUserJobApplications = async (req, res) => {
 };
 
 // Update user profile (resume)
-export const updateUserResume = async (req, res) => {};
+export const updateUserResume = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const resumeFile = req.file;
+    const userData = await User.findById(userId);
+
+    if (resumeFile) {
+      const resumeUpload = await cloudinary.uploader.upload(resumeFile.path);
+      userData.resume = resumeUpload.secure_url;
+    }
+
+    await userData.save();
+    return res.json({ success: true, message: "Resume Updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
