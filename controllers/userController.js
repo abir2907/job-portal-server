c563497d2783b7439a11b3013215c998dd90ed98
Abir Chodha <abirchodha1@gmail.com>
2025-07-10 03:23:24 +0530

    Add user controller func to get user job applications

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,7 +52,27 @@ export const applyForJob = async (req, res) => {
 };
 
 // Get user applied applications
-export const getUserJobApplications = async (req, res) => {};
+export const getUserJobApplications = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+
+    const applications = await JobApplication.find({ userId })
+      .populate("companyId", "name email image")
+      .populate("jobId", "title description location category level salary")
+      .exec();
+
+    if (!applications) {
+      return res.json({
+        success: false,
+        message: "No job applications found for this user.",
+      });
+    }
+
+    return res.json({ success: true, applications });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Update user profile (resume)
 export const updateUserResume = async (req, res) => {};
